a87aa96e7800c0ddf249ad48e08c58125683c5b6
asiamontanari <asiamontanari@yahoo.it>
2025-04-29 09:22:15 +0200

    Delete cambi_clienti.sql

--- cambi_clienti.sql
+++
@@ -1,8 +0,0 @@
-SELECT Clienti.Nome, Stazioni.Nome AS StazioneCambio, Cambi.OrdineCambio, Tratte_Stazioni.OrarioArrivo, Tratte_Stazioni.OrarioPartenza
-FROM Cambi
-JOIN Clienti ON Cambi.ClienteID = Clienti.ClienteID
-JOIN Stazioni ON Cambi.StazioneID = Stazioni.StazioneID
-LEFT JOIN Tratte_Stazioni ON Cambi.StazioneID = Tratte_Stazioni.StazioneID
-ORDER BY Cambi.ClienteID, Cambi.OrdineCambio;
-
-
