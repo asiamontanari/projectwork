f33018e0b293703368e77f904ca44cce76976bf8
asiamontanari <asiamontanari@yahoo.it>
2025-04-29 09:22:44 +0200

    Delete storico_prenot.sql

--- storico_prenot.sql
+++
@@ -1,4 +0,0 @@
-SELECT Prenotazioni.PrenotazioneID, Biglietti.TrattaID, Biglietti.Data, Biglietti.Prezzo, Prenotazioni.Stato
-FROM Prenotazioni
-INNER JOIN Biglietti ON Biglietti.PrenotazioneID = Prenotazioni.PrenotazioneID
-WHERE Prenotazioni.ClienteID = 4;
